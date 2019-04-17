b60b64b26c4498c06097eb68a80b098397281162
niketl16 <niketl16@gmail.com>
2019-04-18 02:42:10 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE>
+<html>
+	<head>
+		<title>My online shop</title>
+        
+        
+        <link rel="stylesheet" href="styles/styles.css">
+	</head>
+<body>
+
+	<div class="main_wrapper">
+
+		<div>This is header!</div>
+
+		<div>This is menubar</div>
+
+		<div>This is sidebar</div>
+
+		<div>This is Content area</div>
+	
+		<div>This is footer</div>
+
+
+
+
+
+
+
+
+
+	</div>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
